8cdd9f9acc010105b6fe69687fafb3f599414173
Shubham Mhaske <shubham.mh96@gmail.com>
2020-04-30 22:05:28 +0530

    Updates

---
+++ precache-manifest.2465eed9fa8896144f504cdbdb2c9069.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f1ba1492dc7434d52df106fc52bffb7",
+    "url": "/ReactJS-blog/index.html"
+  },
+  {
+    "revision": "ae3014efd62426cc7ccd",
+    "url": "/ReactJS-blog/static/css/main.3092266e.chunk.css"
+  },
+  {
+    "revision": "3233468e372b9de9ed43",
+    "url": "/ReactJS-blog/static/js/2.958b0b30.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ReactJS-blog/static/js/2.958b0b30.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae3014efd62426cc7ccd",
+    "url": "/ReactJS-blog/static/js/main.b55cae20.chunk.js"
+  },
+  {
+    "revision": "05787b28e2e941e5ea5c",
+    "url": "/ReactJS-blog/static/js/runtime-main.bd18ba90.js"
+  },
+  {
+    "revision": "02240def400f525a9c74534ca66d1de7",
+    "url": "/ReactJS-blog/static/media/Hampi.02240def.jpg"
+  },
+  {
+    "revision": "ba2dd8ca6e78c25c6fb2d43845a0a5f3",
+    "url": "/ReactJS-blog/static/media/Jaisalmer.ba2dd8ca.jpg"
+  },
+  {
+    "revision": "3b2f6c659c2542d71eb43e2e99162bf0",
+    "url": "/ReactJS-blog/static/media/Khajuraho.3b2f6c65.jpg"
+  },
+  {
+    "revision": "0f0137b1f5c3bdf518a259d9d7ed58b4",
+    "url": "/ReactJS-blog/static/media/Mysore-Palace.0f0137b1.jpg"
+  },
+  {
+    "revision": "ab7ce33bd0047d2b97f01c0386fe11b0",
+    "url": "/ReactJS-blog/static/media/Shillong.ab7ce33b.jpg"
+  },
+  {
+    "revision": "c68e18ecd0dd65be4074b0d51a3ec6c0",
+    "url": "/ReactJS-blog/static/media/ajanta-and-ellora-caves.c68e18ec.jpg"
+  },
+  {
+    "revision": "880f1f1ae76564c48f742937dbcfc584",
+    "url": "/ReactJS-blog/static/media/goa.880f1f1a.jpg"
+  },
+  {
+    "revision": "fdbae9189a99f9beaef26117671db7b5",
+    "url": "/ReactJS-blog/static/media/himachal.fdbae918.jpg"
+  },
+  {
+    "revision": "bb57118c6a60b38f7d6fed8eb9e926f8",
+    "url": "/ReactJS-blog/static/media/memories-from.bb57118c.jpg"
+  },
+  {
+    "revision": "b53071c42183e9da542d60adb5c28a54",
+    "url": "/ReactJS-blog/static/media/travel-gifts.b53071c4.jpg"
+  },
+  {
+    "revision": "02595dc92686beebc7a22fe1b89833b6",
+    "url": "/ReactJS-blog/static/media/udaipur.02595dc9.jpg"
+  },
+  {
+    "revision": "a69f7fd0925a1d9b1462a4626333a8ae",
+    "url": "/ReactJS-blog/static/media/varanasi.a69f7fd0.jpg"
+  }
+]);
